f9e45cfaa82b2f843ce21ad96ce4b3ce5bf04da5
Haseeb Mohammed <mrhaseebmohammed@gmail.com>
2015-12-06 19:34:54 -0600

    I don't understand the GitHub extension for Visual Studio.
    I switched to using the GitHub for Windows app.

---
+++ MathRelated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class MathRelated
+    {
+        //Given an array of ints, write a C# method to total all of the values that are even numbers
+        public int ReturnSumOfEvenNumbers(int[] ArrayOfInts)
+        {
+            return ArrayOfInts.Where(x => x % 2 == 0).Sum();
+        }
+    }
+}
