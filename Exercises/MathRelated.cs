fbf604f93bdc55db52c5a2c2cdd15bd6af02e0be
Haseeb Mohammed <mrhaseebmohammed@gmail.com>
2015-12-06 21:00:10 -0600

    Project Euler Problem 1
    Project Euler Problem 1
    If we list all the natural numbers below 10 that are multiples of 3 or
    5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    Find the sum of all the multiples of 3 or 5 below 1000.

--- Exercises/MathRelated.cs
+++ Exercises/MathRelated.cs
@@ -37,6 +37,21 @@ namespace Exercises
                 radius = newRadius;
             }
         }
-        
+
+        //ProjectEuler Problem 1
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+        public int SumOfMultiplesOf3or5(int NumberToFindMultiplesOf)
+        {
+            int sum = 0;
+            for (var ndx = 1; ndx < NumberToFindMultiplesOf; ndx++)
+            {
+                if (ndx % 3 == 0 || ndx % 5 == 0)
+                {
+                    sum += ndx;
+                }
+            }
+            return sum;
+        }       
     }
 }
--- ExercisesTests/MathRelatedTests.cs
+++ ExercisesTests/MathRelatedTests.cs
@@ -141,5 +141,17 @@ namespace Exercises.Tests
             Assert.AreEqual(roundedCircumference, 12.57);
         }
 
+        //ProjectEuler Problem 1
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+        [TestMethod()]
+        public void SumOfMultiplesOf3or5Test()
+        {
+            var x = new MathRelated();
+
+            var sum = x.SumOfMultiplesOf3or5(10);
+
+            Assert.AreEqual(23, sum);
+        }
     }
 }
